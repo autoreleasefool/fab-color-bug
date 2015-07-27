0b6e2a88cb6519181a00dfc8711c7c327c47effe
Joseph Roque <github@josephroque.ca>
2015-07-27 16:15:54 -0400

    Initial project commit

---
+++ FabBug/app/src/main/java/com/example/fabbugapplication/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.fabbugapplication;
+
+import android.content.res.ColorStateList;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity
+        extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Setting colors of first fab
+        FloatingActionButton fab =
+                (FloatingActionButton) findViewById(R.id.fab_1);
+        setFloatingActionButtonColors(fab,
+                getResources().getColor(R.color.blue),
+                getResources().getColor(R.color.blue_dark));
+
+        //Setting colors of second fab
+        fab = (FloatingActionButton) findViewById(R.id.fab_2);
+        setFloatingActionButtonColors(fab,
+                getResources().getColor(R.color.red),
+                getResources().getColor(R.color.red_dark));
+
+        //Setting colors of third fab
+        final FloatingActionButton clickableFab = (FloatingActionButton) findViewById(R.id.fab_3);
+        setFloatingActionButtonColors(clickableFab,
+                getResources().getColor(R.color.green),
+                getResources().getColor(R.color.green_dark));
+        clickableFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+;                setFloatingActionButtonColors(clickableFab,
+                        getResources().getColor(R.color.purple),
+                        getResources().getColor(R.color.purple_dark));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setFloatingActionButtonColors(FloatingActionButton fab,
+                                               int primaryColor,
+                                               int rippleColor)
+    {
+        int[][] states = {
+                {android.R.attr.state_enabled},
+                {android.R.attr.state_pressed},
+        };
+
+        int[] colors = {
+                primaryColor,
+                rippleColor,
+        };
+
+        ColorStateList colorStateList = new ColorStateList(states, colors);
+        fab.setBackgroundTintList(colorStateList);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # fab-color-bug
-Demonstrates a bug in the Android Design Support Library with the FloatingActionButton.
+Demonstrates a bug in the Android Design Support Library with the FloatingActionButton when used with API 22. Setting the color of a `FloatingActionButton` with multiple `FloatingActionButton` views on the screen causes all instances to be displayed with the same color. This effect can also be seen by changing the color in the on click of *one* of the `FloatingActionButton`, then tapping a different button, which causes it to become the same color as the other, without calling any of the same code.
+
+To reproduce:
+
+1. Create a blank Android application.
+2. Add `compile 'com.android.support:design:22.2.1'` to the dependencies.
+3. Declare 2 or more `FloatingActionButton` views in the `activity_main.xml` as follows:
+
+```
+<RelativeLayout
+	xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:tools="http://schemas.android.com/tools"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:paddingLeft="@dimen/activity_horizontal_margin"
+    android:paddingRight="@dimen/activity_horizontal_margin"
+    android:paddingTop="@dimen/activity_vertical_margin"
+    android:paddingBottom="@dimen/activity_vertical_margin">
+
+    <android.support.design.widget.FloatingActionButton
+        android:id="@+id/fab_2"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_centerHorizontal="true"
+        android:layout_margin="16dp"
+        app:borderWidth="0dp"
+        app:elevation="4dp"
+        app:fabSize="normal" />
+
+    <android.support.design.widget.FloatingActionButton
+        android:id="@+id/fab_1"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_margin="16dp"
+        android:layout_toLeftOf="@id/fab_2"
+        app:borderWidth="0dp"
+        app:elevation="4dp"
+        app:fabSize="normal" />
+
+    <android.support.design.widget.FloatingActionButton
+        android:id="@+id/fab_3"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_margin="16dp"
+        android:layout_toRightOf="@id/fab_2"
+        app:borderWidth="0dp"
+        app:elevation="4dp"
+        app:fabSize="normal" />
+
+</RelativeLayout>
+
+```
+
+4. In `MainActivity.java`, set the colors of all 3 `FloatingActionButton` views  to different colors by using the method:
+
+```
+fab.setBackgroundTintList(colorStateList);
+```
+
+5. Run the application, and all `FloatingActionButton` objects will have the same colors.
+
+Screenshots
+-----------
+
+The following screenshots show the bug in action. First, API 22 where the bug occurs, then API 21 and API 19 where the bug has not yet been introduced.
+
+![API 22 Screenshot](api22.png?raw=true)
+
+![API 21 Screenshot](api21.png?raw=true)
+
+![API 19 Screenshot](api19.png?raw=true)
